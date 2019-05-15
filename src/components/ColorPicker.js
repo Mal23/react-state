96526561c67617117b5ddb00229f76b345da2a1e
Mallarie <mallariemilano@gmail.com>
2019-05-14 17:41:41 -0700

    removed => that were preventing the color from displaying

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -5,9 +5,9 @@ import styles from './ColorPicker.css';
 function ColorPicker({ selectColorHandler }) {
   return (
     <section className={styles.ColorPicker}>
-      <button className={styles.red} onClick={() => selectColorHandler.bind(null, 'red')}>Red</button>
-      <button className={styles.yellow} onClick={() => selectColorHandler.bind(null, 'yellow')}>Yellow</button>
-      <button className={styles.blue} onClick={() => selectColorHandler.bind(null, 'blue')}>Blue</button>
+      <button className={styles.red} onClick={selectColorHandler.bind(null, 'red')}>Red</button>
+      <button className={styles.yellow} onClick={selectColorHandler.bind(null, 'yellow')}>Yellow</button>
+      <button className={styles.blue} onClick={selectColorHandler.bind(null, 'blue')}>Blue</button>
     </section>
   );
 }
